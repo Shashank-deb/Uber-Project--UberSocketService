b3976e729f5550ab124982b92f8e5bc721f732e4
Shashank Sharma <sharmashashank446@gmail.com>
2025-12-31 18:53:01 +0530

    Something breaks

--- src/main/java/com/example/ubersocketservice/controller/DriverRequestController.java
+++ src/main/java/com/example/ubersocketservice/controller/DriverRequestController.java
@@ -4,9 +4,11 @@ import com.example.ubersocketservice.dto.RideRequestDTO;
 import com.example.ubersocketservice.dto.RideResponseDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,8 +36,9 @@ public class DriverRequestController {
         simpleMessageTemplate.convertAndSend("/topic/rideRequest", rideRequestDTO);
     }
 
-    @MessageMapping("/rideResponse")
-    public void rideResponseHandler(RideResponseDTO rideResponseDTO){
-        System.out.println(rideResponseDTO.getResponse());
+    @CrossOrigin(origins = "http://127.0.0.1:5500")
+    @MessageMapping("/rideResponse/{userId}")
+    public void rideResponseHandler(@DestinationVariable String userId, RideResponseDTO rideResponseDTO){
+        System.out.println(rideResponseDTO.getResponse()+" "+userId);
     }
 }
